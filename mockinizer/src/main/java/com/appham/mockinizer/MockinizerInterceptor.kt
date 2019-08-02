1e2abbf1fc3b9733d0c239d6a7314e11d2f1f838
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2019-08-02 22:26:07 +0100

    add mockinizer http header

--- mockinizer/src/main/java/com/appham/mockinizer/MockinizerInterceptor.kt
+++ mockinizer/src/main/java/com/appham/mockinizer/MockinizerInterceptor.kt
@@ -1,6 +1,5 @@
 package com.appham.mockinizer
 
-import android.util.Log
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Request
@@ -17,22 +16,24 @@ class MockinizerInterceptor(
 
     override fun intercept(chain: Interceptor.Chain): Response {
 
-        fun isMocked(request: Request) =
-            null != mocks[RequestFilter.from(request)]?.also {
-                mockServer.enqueue(it)
-            }
+        fun findMockResponse(request: Request) = mocks[RequestFilter.from(request)]
 
-        fun Interceptor.Chain.findServer(): HttpUrl = when(isMocked(request())) {
-            true -> request().url.newBuilder()
-                .host(mockServer.hostName)
-                .port(mockServer.port)
+        fun Interceptor.Chain.findServer(): HttpUrl =
+            when (val mockResponse = findMockResponse(request())) {
+                is MockResponse -> {
+                    mockResponse.addHeader(
+                        "Mockinizer",
+                        " <-- Real request ${request().url} is now mocked to $mockResponse"
+                    )
+                    mockServer.enqueue(mockResponse)
+                    request().url.newBuilder()
+                        .host(mockServer.hostName)
+                        .port(mockServer.port)
 //                .scheme("http") //TODO: make http - https configurable
-                .build()
-                .also {
-                    Log.w(javaClass.simpleName, "--> url: ${request().url} mockinized to: $it")
+                        .build()
                 }
-            false -> request().url
-        }
+                else -> request().url
+            }
 
         return with(chain) {
             proceed(
