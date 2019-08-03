d8fd0ec7c525041fbe86e0a8c8f1398d343038d2
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2019-08-03 23:52:09 +0100

    add Kdoc comment for mockinize ext function

--- mockinizer/src/main/java/com/appham/mockinizer/OkHttpClientExt.kt
+++ mockinizer/src/main/java/com/appham/mockinizer/OkHttpClientExt.kt
@@ -5,6 +5,22 @@ import okhttp3.mockwebserver.MockResponse
 import java.security.cert.X509Certificate
 import javax.net.ssl.*
 
+/**
+ * The main function that wires up the [MockWebServer] with [OkHttpClient]. Generally only the
+ * mocks map needs to be defined. The default values for the other params should be fine for most
+ * projects.
+ *
+ * @param mocks Map of RequestFilter / MockResponse Entries to define requests that
+ * should be directed to the mock server instead of the real one. default value is an empty map.
+ * @param trustManagers Array of TrustManager to be used for https connections with mock server
+ * default value is an all trusting manager
+ * @param socketFactory SSLSocketFactory to be used for RetroFit https connections
+ * default value is using the previously defined trustManagers
+ * @param hostnameVerifier HostNameVerifier the interface to be used to verify hostnames.
+ * default value is an all verifying verifier
+ *
+ * @return OkHttpClient.Builder for chaining
+ */
 fun OkHttpClient.Builder.mockinize(
     mocks: Map<RequestFilter, MockResponse> = mapOf(),
     trustManagers: Array<TrustManager> = getAllTrustingManagers(),
