089c76e6993a1b729fc74cbc5badb73335f417a4
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2019-08-04 00:09:29 +0100

    cleanup unused dependencies

--- demo/src/main/java/com/appham/mockinizer/Post.kt
+++ demo/src/main/java/com/appham/mockinizer/Post.kt
@@ -1,13 +1,9 @@
 package com.appham.mockinizer
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity(tableName = "posts")
 data class Post(
 
-    @PrimaryKey
     @SerializedName("id")
     val id: Int,
 
